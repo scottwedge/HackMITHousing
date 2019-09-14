3b825b589fd0470b06e6714523a3258ebe72e207
Holly Zheng <46336328+hollyyuqizheng@users.noreply.github.com>
2019-09-14 14:09:10 -0400

    Update match.py

--- match.py
+++ match.py
@@ -2,16 +2,24 @@
 Toy algorithm for computing score between a landlord's request and a tenant's availability
 
 Output: 
+
 """
 
-NUM_TAG = 5 #ie. length of each user's array
+from datetime import *
+
+NUM_TAG =  #ie. length of each user's array
 
 #map each tag to its index in the array
-WORK_TYPE = 0
-HOURS = 1
-DISTANCE = 2
-NUM_WORKER = 3
-PRICE = 4
+WORK_TYPE = 
+YEAR = 
+MONTH_START = 
+DATE_START = 
+MONTH_END = 
+DATE_END = 
+HOURS = 
+DISTANCE = 
+PRICE = 
+RATE = 
 
 #Value for each work type
 KITCHEN = 0 #kitchen
@@ -20,16 +28,37 @@ FURN = 2 #furniture
 ALL = 3 #entire apartment
 
 
+#landlord: list for curr landlord
+#tenants: list for all tenants
 
-
-tenants = [] #list for all tenants
 res = {}
 
-
-landlord = [0 for i in range(NUM_TAG)] #curr landlord
 
 for tenant in tenants:
 
   #work type doesn't match, return very high diff score
   if landlord[WORK_TYPE] != tenant[WORK_TYPE]:
     return #??
+  
+  #Check for overlap in date
+  l_date_start = datetime.datetime(landlord[YEAR], landlord[MONTH_START], landlord[DATE_START])
+  l_date_end = datetime.datetime(landlord[YEAR], landlord[MONTH_END], landlord[DATE_END])
+  t_date_start = datetime.datetime(tenant[YEAR], tenant[MONTH_START], tenant[DATE_START])
+  t_date_end = datetime.datetime(tenant[YEAR], tenant[MONTH_END], tenant[DATE_END])
+  
+  if not (t_date_start >= l_date_start or t_date_end <= l_date_end): #no overlap front or back
+    return #??
+  
+  
+  #check for rating expectation
+  if tenant[RATE] < landlord[RATE]:
+    return #??
+  
+  #diff in expectation of hours
+  hour_diff = abs(landlord[HOURS] - tenant[HOURS]) 
+  
+  #diff in location distance
+  dist_diff = abs(landlord[DIST] - tenant[DIST])
+  
+  
+  
