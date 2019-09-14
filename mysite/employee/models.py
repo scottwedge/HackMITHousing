639263bc0d94f6d6041003e35905905f09bf27d1
Holly Zheng <46336328+hollyyuqizheng@users.noreply.github.com>
2019-09-14 16:44:28 -0400

    Update models.py

--- mysite/employee/models.py
+++ mysite/employee/models.py
@@ -6,24 +6,31 @@ from calendar immport monthrange
 GENDERS = [('male', 'MALE'), ('female', 'FEMALE')]
 WORK_TYPES = [('kitchen', 'KITCHEN'), ('bathroom', 'BATHROOM'), ('furniture', 'FURNITURE'), ('all', 'ALL')]
 
+#MONTHS = [('January', 'JAN'), ('February','FEB'),('March','MAR'),('April','APR'),('May','MAY'),('June','JUN'),('July','JUL'),
+#         ('August','AUG'),('September','SEP'),('October','OCT'),('November','NOV'),('December','DEC')]
+
 class Employee(models.Model):
   
   employee_id = models.AutoField(primary_key=True)
   name = models.CharField(max_length=200)
   gender = models.ChoiceField(choices=GENDERS)
   
+  start_date = models.DateField(input_format='%Y-%m-%d', initial=datetime.date.today)
+  end_date = models.DateField(input_format='%Y-%m-%d', initial=datetime.date.today)
   
-  #How to parse date format into a uniform one??
+  
+  """
   start_year = models.IntegerField(min_value = datetime.now().year, 
                                    max_value = datetime.now().year + 1)
-  start_month = models.IntegerField(min_value=1, max_value=12)
+  start_month = models.ChoiceField(choices=MONTHS)
   start_date = models.IntegerField(min_value=1, max_value=monthrange(datetime.now().year, start_month)[1])
   
   
   end_year = models.IntegerField(min_value = datetime.now().year, 
                                    max_value = datetime.now().year + 1)
-  end_month = models.IntegerField(min_value=1, max_value=12)
+  end_month = models.ChoiceField(choices=MONTHS)
   end_date = models.IntegerField(min_value=1, max_value=monthrange(datetime.now().year, end_month)[1])
+  """
   
   
   
