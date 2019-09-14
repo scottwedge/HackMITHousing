81f8a5892b7c949c85617a141890509c9cc3d2c9
Holly Zheng <46336328+hollyyuqizheng@users.noreply.github.com>
2019-09-14 16:10:36 -0400

    Update models.py

--- mysite/employee/models.py
+++ mysite/employee/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+GENDERS = [('male', 'MALE'), ('female', 'FEMALE')]
+
+class Employee(models.Model):
+  name = models.CharField(max_length=200)
+  gender = models.ChoiceField(choices=GENDERS)
