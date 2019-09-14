4f1fd07671aa2b51f1d536d897afb37c8c3a5297
Holly Zheng <46336328+hollyyuqizheng@users.noreply.github.com>
2019-09-14 13:24:06 -0400

    Update match.py

--- match.py
+++ match.py
@@ -1,3 +1,17 @@
 """
-
+Toy algorithm for computing score between a landlord's request and a tenant's availability
 """
+
+#map each tag to its index in the array
+WORK_TYPE = 0
+HOURS = 1
+DISTANCE = 2
+AREA = 3
+NUM_WORKER = 4
+
+#Value for each work type
+KITCHEN = 0 #kitchen
+BATH = 1 #bathroom
+FURN = 2 #furniture
+ALL = 3 #entire apartment
+
