cc8c7a5c277ee0904114824dc19bea15b3d33333
Holly Zheng <46336328+hollyyuqizheng@users.noreply.github.com>
2019-09-14 16:47:28 -0400

    Update models.py

--- mysite/employer/models.py
+++ mysite/employer/models.py
@@ -17,22 +17,6 @@ class Employer(models.Model):
   start_date = models.DateField(input_format='%Y-%m-%d', initial=datetime.date.today)
   end_date = models.DateField(input_format='%Y-%m-%d', initial=datetime.date.today)
   
-  
-  """
-  start_year = models.IntegerField(min_value = datetime.now().year, 
-                                   max_value = datetime.now().year + 1)
-  start_month = models.ChoiceField(choices=MONTHS)
-  start_date = models.IntegerField(min_value=1, max_value=monthrange(datetime.now().year, start_month)[1])
-  
-  
-  end_year = models.IntegerField(min_value = datetime.now().year, 
-                                   max_value = datetime.now().year + 1)
-  end_month = models.ChoiceField(choices=MONTHS)
-  end_date = models.IntegerField(min_value=1, max_value=monthrange(datetime.now().year, end_month)[1])
-  """
-  
-  
-  
   work_type = models.ChoiceField(choices=WORK_TYPES)
   max_hours = models.IntegerField(min_value=1, max_value=24)
   city = models.CharField(max_length=100)
